c45be6e0628fe4c68af995525f8783ee6b15afc2
nishitaku <29690066+nishitaku@users.noreply.github.com>
2021-05-07 23:30:55 +0900

    implements sample code

--- src/app/pages/first/first.component.ts
+++ src/app/pages/first/first.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { LogService } from 'src/app/services/log/log.service';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss'],
 })
-export class FirstComponent implements OnInit {
-  constructor() {}
+export class FirstComponent implements OnInit, DoCheck {
+  text?: string;
 
-  ngOnInit(): void {}
+  constructor(private logService: LogService) {}
+
+  ngOnInit(): void {
+    this.logService.add('FirstComponent: ngOnInit');
+    this.text = 'start';
+  }
+
+  ngDoCheck(): void {
+    this.logService.add('FirstComponent: ngDoCheck');
+  }
+
+  getBgColor(): string {
+    this.logService.add('FirstComponent: getBgColor');
+    return 'green';
+  }
+
+  onClickChangeText(): void {
+    this.text = 'changed';
+  }
 }
