37048d375c825ab0ee51341865d65cadbe5119a7
nishitaku <29690066+nishitaku@users.noreply.github.com>
2021-05-11 14:58:48 +0900

    add change string sample

--- src/app/pages/first/component/first-one/first-one.component.ts
+++ src/app/pages/first/component/first-one/first-one.component.ts
@@ -1,10 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { LogService } from 'src/app/services/log/log.service';
 @Component({
   selector: 'app-first-one',
   templateUrl: './first-one.component.html',
   styleUrls: ['./first-one.component.scss'],
 })
-export class FirstOneComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+export class FirstOneComponent implements OnInit, OnChanges {
+  @Input() inputStr!: string;
+
+  constructor(private logService: LogService) {}
+
+  ngOnInit(): void {
+    this.logService.add(this, `ngOnInit fired.`);
+    if (this.inputStr == null) {
+      throw new Error('[inputStr] is required.');
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.logService.add(this, `ngOnChanges fired. ${JSON.stringify(changes)}`);
+  }
 }
--- src/app/pages/first/first.component.ts
+++ src/app/pages/first/first.component.ts
@@ -13,21 +13,20 @@ import { LogService } from 'src/app/services/log/log.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FirstComponent implements OnInit, DoCheck {
-  text?: string;
+  text: string = 'init';
 
   constructor(private logService: LogService) {}
 
   ngOnInit(): void {
     this.logService.add(this, `ngOnInit fired.`);
-    this.text = 'start';
   }
 
   ngDoCheck(): void {
-    this.logService.add(this, `ngDoCheck fired.`);
+    // this.logService.add(this, `ngDoCheck fired.`);
   }
 
   getBgColor(): string {
-    this.logService.add(this, `getBgColor fired.`);
+    // this.logService.add(this, `getBgColor fired.`);
     return 'green';
   }
 
